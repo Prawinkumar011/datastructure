1637106026e724d4bccfd3272d0a5aec68e287c9
Prawinkumar011 <114295435+Prawinkumar011@users.noreply.github.com>
2023-07-26 21:22:49 +0530

    Add files via upload

---
+++ Day 5/main 1.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int vertex;
+    struct Node* next;
+} Node;
+
+typedef struct Graph {
+    int numVertices;
+    Node** adjLists;
+} Graph;
+
+// Function to create a new node
+Node* createNode(int v) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to create a graph with a given number of vertices
+Graph* createGraph(int numVertices) {
+    Graph* graph = (Graph*)malloc(sizeof(Graph));
+    graph->numVertices = numVertices;
+    graph->adjLists = (Node**)malloc(numVertices * sizeof(Node*));
+
+    for (int i = 0; i < numVertices; i++) {
+        graph->adjLists[i] = NULL;
+    }
+
+    return graph;
+}
+
+// Function to add a directed edge to the graph
+void addEdge(Graph* graph, int src, int dest) {
+    Node* newNode = createNode(dest);
+    newNode->next = graph->adjLists[src];
+    graph->adjLists[src] = newNode;
+}
+
+// Function to print the adjacency list representation of the graph
+void printGraph(Graph* graph) {
+    for (int i = 0; i < graph->numVertices; i++) {
+        printf("Adjacency list of vertex %d: ", i);
+        Node* temp = graph->adjLists[i];
+        while (temp != NULL) {
+            printf("%d -> ", temp->vertex);
+            temp = temp->next;
+        }
+        printf("NULL\n");
+    }
+}
+
+// Function to free the memory used by the graph
+void freeGraph(Graph* graph) {
+    if (graph == NULL) {
+        return;
+    }
+
+    for (int i = 0; i < graph->numVertices; i++) {
+        Node* temp = graph->adjLists[i];
+        while (temp != NULL) {
+            Node* next = temp->next;
+            free(temp);
+            temp = next;
+        }
+    }
+
+    free(graph->adjLists);
+    free(graph);
+}
+
+int main() {
+    int numVertices = 5;
+    Graph* graph = createGraph(numVertices);
+
+    addEdge(graph, 0, 1);
+    addEdge(graph, 0, 4);
+    addEdge(graph, 1, 2);
+    addEdge(graph, 1, 3);
+    addEdge(graph, 2, 3);
+    addEdge(graph, 3, 4);
+
+    printGraph(graph);
+
+    freeGraph(graph);
+
+    return 0;
+}
---
+++ Day 5/main.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int vertex;
+    struct Node* next;
+} Node;
+
+typedef struct Graph {
+    int numVertices;
+    Node** adjLists;
+} Graph;
+
+// Function to create a new node
+Node* createNode(int v) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to create a graph with a given number of vertices
+Graph* createGraph(int numVertices) {
+    Graph* graph = (Graph*)malloc(sizeof(Graph));
+    graph->numVertices = numVertices;
+    graph->adjLists = (Node**)malloc(numVertices * sizeof(Node*));
+
+    for (int i = 0; i < numVertices; i++) {
+        graph->adjLists[i] = NULL;
+    }
+
+    return graph;
+}
+
+// Function to add an edge to an undirected graph
+void addEdge(Graph* graph, int src, int dest) {
+    // Add an edge from src to dest
+    Node* newNode = createNode(dest);
+    newNode->next = graph->adjLists[src];
+    graph->adjLists[src] = newNode;
+
+    // Add an edge from dest to src (since the graph is undirected)
+    newNode = createNode(src);
+    newNode->next = graph->adjLists[dest];
+    graph->adjLists[dest] = newNode;
+}
+
+// Function to print the adjacency list representation of the graph
+void printGraph(Graph* graph) {
+    for (int i = 0; i < graph->numVertices; i++) {
+        printf("Adjacency list of vertex %d: ", i);
+        Node* temp = graph->adjLists[i];
+        while (temp != NULL) {
+            printf("%d -> ", temp->vertex);
+            temp = temp->next;
+        }
+        printf("NULL\n");
+    }
+}
+
+// Function to free the memory used by the graph
+void freeGraph(Graph* graph) {
+    if (graph == NULL) {
+        return;
+    }
+
+    for (int i = 0; i < graph->numVertices; i++) {
+        Node* temp = graph->adjLists[i];
+        while (temp != NULL) {
+            Node* next = temp->next;
+            free(temp);
+            temp = next;
+        }
+    }
+
+    free(graph->adjLists);
+    free(graph);
+}
+
+int main() {
+    int numVertices = 5;
+    Graph* graph = createGraph(numVertices);
+
+    addEdge(graph, 0, 1);
+    addEdge(graph, 0, 4);
+    addEdge(graph, 1, 2);
+    addEdge(graph, 1, 3);
+    addEdge(graph, 1, 4);
+    addEdge(graph, 2, 3);
+    addEdge(graph, 3, 4);
+
+    printGraph(graph);
+
+    freeGraph(graph);
+
+    return 0;
+}
